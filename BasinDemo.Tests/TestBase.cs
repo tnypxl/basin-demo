ec53f3a44c6ef92dabb86a752edf6d25e6c50905
Arik Jones <tnypxl00@icloud.com>
2020-06-25 10:33:15 -0500

    Finish tests

--- BasinDemo.Tests/TestBase.cs
+++ BasinDemo.Tests/TestBase.cs
@@ -2,18 +2,35 @@ using System.IO;
 using Basin;
 using NUnit.Framework;
 using Basin.Selenium;
+using Basin.Selenium.Builders;
+using Basin.Config.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace BasinDemo.Tests
 {
     public class TestBase
     {
+
+        private readonly IDriverConfig _driverConfig;
+
         [SetUp]
         public void SetUp()
         {
             var CONFIG_PATH = Path.GetFullPath("../../../") + "BasinDemo.json";
 
             BSN.SetConfig(CONFIG_PATH);
-            Driver.Init();
+
+            Driver.Init(() =>
+            {
+
+                var builder = new FirefoxBuilder(_driverConfig);
+                builder.DriverOptions.PageLoadStrategy = PageLoadStrategy.Eager;
+                builder.DriverOptions.LogLevel = FirefoxDriverLogLevel.Debug;
+                builder.SetHeadless();
+                return builder;
+
+            });
             Driver.Goto(BSN.Config.Site.Url);
             Pages.Init();
         }
