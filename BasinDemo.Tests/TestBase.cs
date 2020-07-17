cca73bc6f248794bbc66e10e167ed9e71014d00b
Arik Jones <tnypxl00@icloud.com>
2020-07-17 17:04:53 -0500

    Resolve conflict

--- BasinDemo.Tests/TestBase.cs
+++ BasinDemo.Tests/TestBase.cs
@@ -13,15 +13,21 @@ namespace BasinDemo.Tests
             var CONFIG_PATH = Path.GetFullPath("../../../") + "BasinDemo.json";
 
             BasinEnv.SetConfig(CONFIG_PATH);
+<<<<<<< Updated upstream
             Browser.Init();
             Browser.Goto(BasinEnv.Site.Url);
+=======
+
+            BrowserSession.Init();
+            BrowserSession.Goto(BasinEnv.Site.Url);
+>>>>>>> Stashed changes
             Pages.Init();
         }
 
         [TearDown]
         public void Teardown()
         {
-            Browser.Current?.Quit();
+            BrowserSession.Current?.Quit();
         }
     }
 }
